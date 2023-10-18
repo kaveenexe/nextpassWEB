e9302dbc649a514fbbc8293f72ea2ef20655d270
Ashani Rasangika Dewmini <it21016820@my.sliit.lk>
2023-10-18 17:45:36 +0530

    create home page

---
+++ frontend/src/components/banner/banner.jsx
@@ -0,0 +1,37 @@
+import Carousel from 'react-bootstrap/Carousel';
+import banner1 from "../../images/homebus1.jpg";
+import banner3 from "../../images/homebus3.jpg";
+import './banner.css';
+
+function HomeBanner() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="custom-img"
+          src={banner1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Explore, Connect, Travel Together!</h3>
+          <p>For Everyone, Anywhere Your Journey, Our Destination</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      
+      <Carousel.Item>
+        <img
+          className="custom-img"
+          src={banner3}
+          alt="Thrid slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Welcome to NextPass</h3>
+          <p>Experience the Magic of Sri Lanka - A Journey to Remember!</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default HomeBanner;
