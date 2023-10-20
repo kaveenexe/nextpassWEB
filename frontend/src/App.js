d14b097d1a16d7eff09332151d8e84192d921acf
Ashani Rasangika Dewmini <it21016820@my.sliit.lk>
2023-10-20 13:50:15 +0530

    create login page frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/home";
-
+import Login from "./pages/Login/login";
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Home />} />
-
-
-
-      </Routes>
-      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
-
-     
     </div>
   );
 }
